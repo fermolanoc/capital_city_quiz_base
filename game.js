28e32ffb0fed0a67f3218b3451ef528d10584a5e
Fernando Molano <fm87usa@gmail.com>
2021-02-27 19:02:44 -0600

    add play-again button, create function to generate randon countries, fix the way random country name was being generated, add styles to webpage, add number of questions, correct answers and points counters, points message color change depending on quantity, disable input text for user answer once 'check answer' button has been clicked to prevent answer modifications

--- game.js
+++ game.js
@@ -2,49 +2,56 @@ let randomCountryElement = document.querySelector('#random-country')
 let userAnswerElement = document.querySelector("#user-answer")
 let submitButton = document.querySelector("#submit-answer")
 let resultTextElement = document.querySelector('#result')
+let numberOfQuestionsElement = document.querySelector("#number-questions");
+let pointsElement = document.querySelector("#points");
+let playButton = document.querySelector("#play-again");
 
-// TODO finish the script to challenge the user about their knowledge of capital cities.
-// An array of country codes is provided in the countries.js file. 
-// Your browser treats all JavaScript files as one big file, o
-// organized in the order of the script tags so the countriesAndCodes array is available to this script.
+// console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
 
-console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
+
+let randomCountry;
+let countryCode;
+let randomCountryName;
 
 // generate a random number between 0 and the length of the countriesAndCodes array.
 // This number will be used as an index when getting random countries
-let randomNumber = Math.floor(Math.random() * countriesAndCodes.length);
-let randomCountry = countriesAndCodes[randomNumber].name; // get a random country from array
+function generateRandomCountry() {
+    let randomNumber = Math.floor(Math.random() * countriesAndCodes.length);
+    randomCountry = countriesAndCodes[randomNumber]; // get a random country from array
+    randomCountryName = randomCountry.name
+    countryCode = randomCountry["alpha-2"];
+    // console.log(randomCountryName);
+    // console.log(countryCode);
+    return randomCountryName;
+}
+
+// print the random country name on the webpage
+function getCountryName() {
+    randomCountryElement.innerHTML = generateRandomCountry(); // show the random country on webpage
+}
 
 
 // when the page loads, select an element at random from the countriesAndCodes array
 window.addEventListener("load", () => {
-    randomCountryElement.innerHTML = randomCountry; // show the random country on webpage
+    getCountryName(); // call function to print random country's name on webpage
+
 })
 
 
-// TODO add a click event handler to the submitButton.  When the user clicks the button,
-//  * read the text from the userAnswerElement 
-//  * Use fetch() to make a call to the World Bank API with the two-letter country code (from countriesAndCodes, example 'CN' or 'AF')
-//  * Verify no errors were encountered in the API call. If an error occurs, display an alert message. 
-//  * If the API call was successful, extract the capital city from the World Bank API response.
-//  * Compare it to the user's answer. 
-//      You can decide how correct you require the user to be. At the minimum, the user's answer should be the same
-//      as the World Bank data - make the comparison case insensitive.
-//      If you want to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
-//  * Finally, display an appropriate message in the resultTextElement to tell the user if they are right or wrong. 
-//      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
+
+// count the number of questions and points collected by user
+let numberOfQuestions = 0;
+let correctAnswers = 0;
+let points = 0;
 
 // Click event handler for 'Check Answer' button
 submitButton.addEventListener("click", () => {
     let userAnswer = userAnswerElement.value; // save user answer into variable
 
-    // get official code for country randomly shown
-    let countryCode = countriesAndCodes[randomNumber]["alpha-2"];
-    console.log(countryCode);
-
 
     // generate random API based on the country generated passing the countryCode
     let url = `https://api.worldbank.org/v2/country/${countryCode}?format=json`;
+
     // fetch json data from API url generated for the chosen country
     fetch(url)
         .then(res => {
@@ -53,19 +60,57 @@ submitButton.addEventListener("click", () => {
         .then(data => { // assign json results to 'data' variable
             let countryCapital = data[1][0].capitalCity; // get capital value of the random country
 
+            numberOfQuestions++; // increase number of questions on 1
+            userAnswerElement.setAttribute("disabled", true); // disable input element so user cannot modify answer
+
+
             // convert both user answer and capital city of the random country to lowercase to compare them
             // show message indicating wether answer is correct or wrong
             if (userAnswer.toLowerCase() === countryCapital.toLowerCase()) {
-                resultTextElement.innerHTML = `Correct! The capital of ${randomCountry} is ${countryCapital}`;
+                points++; // if answer was correct, increase points on 1
+                correctAnswers++; // increase correctAnswers on 1
+
+                // print message on webpage
+                resultTextElement.innerHTML = `Correct! The capital of ${randomCountryName} is ${countryCapital}`;
+                numberOfQuestionsElement.innerHTML = `Questions: ${numberOfQuestions}<br/>Correct: ${correctAnswers}`;
+                pointsElement.innerHTML = `Points: ${points}`;
             } else {
-                resultTextElement.innerHTML = `Wrong! The capital of ${randomCountry} is not ${userAnswer}; It is ${countryCapital}`;
+                points-- // if answer was wrong, decrease points on 1
+
+                // print message on webpage
+                resultTextElement.innerHTML = `Wrong! The capital of ${randomCountryName} is not ${userAnswer}; It is ${countryCapital}`;
+                numberOfQuestionsElement.innerHTML = `Questions: ${numberOfQuestions}<br/>Correct: ${correctAnswers}`;
+                pointsElement.innerHTML = `Points: ${points}`;
+            }
+
+            // style points message
+            if (points < 0) {
+                pointsElement.style.color = "tomato"
+            } else if (points == 0) {
+                pointsElement.style.color = "black"
+            } else {
+                pointsElement.style.color = "blueviolet"
             }
         })
         .catch(err => {
             alert("An error occurred getting the country's info", err);
+            console.log(err);
         })
 })
 
-// TODO finally, connect the play again button. Clear the user's answer, select a new random country, 
-// display the country's name, handle the user's guess. If you didn't use functions in the code you've 
-// already written, you should refactor your code to use functions to avoid writing very similar code twice. 
+
+// Click event handler for play-again button
+playButton.addEventListener("click", () => {
+    // generate a new random country
+    generateRandomCountry();
+
+    // show it in the webpage
+    getCountryName();
+
+    // enable user to type an answer again
+    userAnswerElement.removeAttribute("disabled");
+
+    // clear user's answer and the result message
+    userAnswerElement.value = "";
+    resultTextElement.innerHTML = "";
+})
